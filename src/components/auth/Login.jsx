3bcbcbfaddf6fc9e860400b2cd288b0fdb5183c0
adityx-reposit <adityayadav2022@vitbhopal.ac.in>
2025-02-09 23:06:08 +0530

    initial

---
+++ src/components/auth/Login.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div className="flex items-center justify-center bg-red-500">
+      <div className="border-2 border-red-600 p-4">
+        <form action="">
+          <h1 className="text-black">Login</h1>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
